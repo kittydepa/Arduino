671fd8aa65a74a67252883bdec0e3be2c68d5b96
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-08-25 09:56:46 +0200

    Project 4 - Colour Mixing Lamp

---
+++ project4/project4.ino
@@ -0,0 +1,62 @@
+const int greenLEDPin = 9;
+const int redLEDPin = 10;
+const int blueLEDPin = 11;
+
+const int redSensorPin = A0;
+const int greenSensorPin = A1;
+const int blueSensorPin = A2;
+
+// Variables to store the sensor readings and light level of each LED
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+int redSensorValue = 0;
+int greenSensorValue = 0;
+int blueSensorValue = 0;
+
+
+void setup() {
+  // Use 9600 bps to begin serial communication
+  Serial.begin(9600);
+  // Define LED pin outputs as the following
+  pinMode(greenLEDPin, OUTPUT);
+  pinMode(redLEDPin, OUTPUT);
+  pinMode(blueLEDPin, OUTPUT);
+}
+
+void loop() {
+  // Read the value of each light sensor, and add a small delay
+  redSensorValue = analogRead(redSensorPin);
+  delay(5);
+  greenSensorValue = analogRead(greenSensorPin);
+  delay(5);
+  blueSensorValue - analogRead(blueSensorPin);
+
+  // Print out the sensor readings
+  Serial.print("Raw Sensor Values \t red: ");
+  Serial.print(redSensorValue);
+  Serial.print("\t green: ");
+  Serial.print(greenSensorValue);
+  Serial.print("\t blue: ");
+  Serial.println(blueSensorValue);
+
+  // Converting the sensor reading. analogWrite() is what changes the LED's brightness via PWM, and you will need to divide by 4 to get the duty cycle
+  redValue = redSensorValue/4;
+  greenValue = greenSensorValue/4;
+  blueValue = blueSensorValue/4;
+
+  // Report the new calculated LED light levelts
+  Serial.print("Mapped Sensor Values \t red: ");
+  Serial.print(redValue);
+  Serial.print("\t green: ");
+  Serial.print(greenValue);
+  Serial.print("\t blue: ");
+  Serial.println(blueValue);
+
+  // Set LED light levels
+  analogWrite(redLEDPin, redValue);
+  analogWrite(greenLEDPin, greenValue);
+  analogWrite(blueLEDPin, blueValue);
+
+}
