998c70eead33db1cef2f439312931fdc57dbeda7
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-12-24 20:47:32 +0100

    Success - but only green LED blinking

--- xmas_demo/xmas_demo.ino
+++ xmas_demo/xmas_demo.ino
@@ -1,30 +1,42 @@
 /*
-* scrollingHelloWorld
-* Author: Darrell Little
-* Date: 10/15/2019
-* This code example shows how to scroll a line of text
+* Originally from: scrollingHelloWorld
+* Original Author: Darrell Little
+* Original Date: 10/15/2019
+* This code was originally an example shows how to scroll a line of text
 * across the screen on an Arduboy.
 * The OLED screen dimensions are 128 x 60 pixels (width x height)
+* It has since been modified by myself (Kitty Depa) as a small Xmas exercise for my own learning.
 */
+
+
 #include <Arduboy2.h>
 #include <Arduboy2Core.h>
  
 Arduboy2 arduboy;
-
 
 // Create the variables needed
 int x;
 int y;
 char* myGreeting = ("Merry Christmas!!! <3 <3 ~ Kitty");
 
+// Variables will change (?)
+int ledState = LOW;                     // ledState used to set the LED
+
+// This is for setting up the LEDs so that they can 'blink without delay'
+unsigned long previousMillis = 0;     // will store last time LED was updated
+const long interval = 300;          // interval at which to blink (in milliseconds)
+
+
+// ------------------------------------------------------- Setup -------------------------------------------------------
 void setup() {
   arduboy.begin();
   arduboy.setFrameRate(30);
-//  arduboy.digitalWriteRGB(RGB_OFF, RGB_OFF, RGB_OFF);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
 }
 
+
+// ------------------------------------------------------- Main loop -------------------------------------------------------
 void loop() {
   if (!(arduboy.nextFrame()))
     return;
@@ -47,13 +59,25 @@ void loop() {
   
   x--;
 
-  digitalWrite(GREEN_LED, LOW); // turn on green LED
-  digitalWrite(RED_LED, HIGH); // turn off red LED
-  delay(1000);
-  digitalWrite(GREEN_LED, HIGH); // turn off green LED
-  digitalWrite(RED_LED, LOW); // turn on red LED
-  delay(1000);
+    // check to see if it's time for the LED to blink, if the diff. between the current and last time the LED blink 
+  // is greater than the interval at which you want it to blink.
+  unsigned long currentMillis = millis();
   
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time the LED blinked
+    previousMillis = currentMillis;
 
+    // if the LED is off turn it on, and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable
+    digitalWrite(GREEN_LED, ledState);
   }
+ 
+ }
+ 
 }
