e914565a6fe674fbd2cb780b3498c519109a35b5
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2022-01-27 20:37:49 +0100

    Tutorial pt 5 - Number guessing game

--- guessin_game/guessin_game.ino
+++ guessin_game/guessin_game.ino
@@ -1,12 +1,125 @@
-// Part 5 of Make Your Own Arduboy Game Tutorial 
+// Part 5 of Make Your Own Arduboy Game Tutorial (from user: crait)
 // Guessing game
+// A number guessing game where Arduboy picks a random number and the player has 7 attempts to guess the number.
+// After each guess, the Arduboy will tell the player a hint. If the player guesses right, they win!
+
+
+
+
+// Libraries to include
+#include <Arduboy2.h>
+
+// Setting up variables
+Arduboy2 arduboy;
+int playerwin;
+int attempts;
+int guessednumber;
+int randomnumber;
+int lastguess;
+
 
 void setup() {
-  // put your setup code here, to run once:
+  arduboy.begin();
+  arduboy.clear();
+
+  playerwin = 0;
+  attempts = 0;
+  guessednumber = 0;
+  arduboy.initRandomSeed();
+  randomnumber = random(1, 101);
+  
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  arduboy.clear();
+  arduboy.pollButtons();
+  
+  // Game code goes here
+  if (playerwin == 0) {
+    // Ask the player for a number and play the game
+ 
+    if (attempts == 7) {
+      // Game Over
+      arduboy.setCursor(0, 0);
+      arduboy.print("You lost!");
+      arduboy.print("\n");
+      arduboy.print("Correct number: ");
+      arduboy.print(randomnumber);
+      arduboy.print("\n");
+    arduboy.print("Press A to play again!");
+      
+      if (arduboy.justPressed(A_BUTTON)) {
+        randomnumber = random(1, 101);
+        attempts = 0;
+        playerwin = 0;
+      }
+    }
+    
+    else { // Player has more attempts
+      if (arduboy.justPressed(UP_BUTTON)) {
+        guessednumber = guessednumber + 1;
+      }
+
+      if (arduboy.justPressed(DOWN_BUTTON)) {
+        guessednumber = guessednumber - 1;
+      }
+
+      if (arduboy.justPressed(A_BUTTON)) {
+        // Guess number
+        if (guessednumber == randomnumber) {
+          playerwin = 1;
+        }
+
+        else {
+          attempts = attempts + 1;
+                     lastguess = guessednumber;
+        }
+      }
+      
+    arduboy.setCursor(0, 0);
+    arduboy.print("Guess the number \n");
+    arduboy.print("between 1-100.\n");
+    arduboy.print("Press A to submit. \n");
+    arduboy.print("\n");
+    arduboy.print("\n");
+    arduboy.print("Attempt: ");
+    arduboy.print(attempts);
+    arduboy.print("\n");
+    arduboy.print("Number to guess: ");
+    arduboy.print(guessednumber);
+    arduboy.print("\n");
+    if (attempts == 0) {
+      arduboy.print("Good luck!");
+    }
+    else {
+      arduboy.print(lastguess);
+      if (lastguess > randomnumber) {
+        arduboy.print(" is too high!");
+      }
+      if (lastguess < randomnumber) {
+        arduboy.print(" is too low!");
+      }
+    } 
+  }
+  }
+
+  else { 
+    // Tell the player that they won!
+    arduboy.setCursor(0, 0);
+    arduboy.print("You won!");
+    arduboy.print("\n");
+    arduboy.print("Correct Number: ");
+    arduboy.print(randomnumber);
+    arduboy.print("Press A to play again!");
+    
+    if (arduboy.justPressed(A_BUTTON)) {
+        randomnumber = random(1, 101);
+        attempts = 0;
+        playerwin = 0;
+      }
+  }
+
+  arduboy.display();
 }
