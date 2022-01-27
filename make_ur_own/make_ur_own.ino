79307eec5f4311be72602cb3688ecdbc2afbb9aa
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2022-01-27 17:22:03 +0100

    Tutorial step 3 - adding a counter

--- make_ur_own/make_ur_own.ino
+++ make_ur_own/make_ur_own.ino
@@ -1,19 +1,28 @@
 // Tutorial from Arduboy Community user: crait 
 
 
+// Including necessary libraries
 #include <Arduboy2.h>
+
+
+// Initializing variables 
 Arduboy2 arduboy;
+int counter;
+
 
 void setup() {
   arduboy.begin();
   arduboy.clear();
-  arduboy.print("Pusheen the cat!");
-  arduboy.display();
+  counter = 0;
 
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  arduboy.clear();
+  arduboy.setCursor(0,0);
+  counter = counter + 1;
+  arduboy.print(counter);
+  arduboy.display();
 
 }
