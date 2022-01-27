acb2db7a7465f6f26ed5d7a6271b2faf4e041402
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2022-01-27 17:38:15 +0100

    Tutorial part 4 - adding conditional statments to counter

--- make_ur_own/make_ur_own.ino
+++ make_ur_own/make_ur_own.ino
@@ -20,8 +20,24 @@ void setup() {
 
 void loop() {
   arduboy.clear();
+  
+  if ( arduboy.pressed(UP_BUTTON) ){
+    // Increase counter
+    counter = counter + 1;
+  }
+
+  if ( arduboy.pressed(DOWN_BUTTON ) ){
+    // Decrease counter
+    counter = counter - 1;
+  }
+
+  // Check if counter is equal to 36
+  if ( counter == 36 ){
+    arduboy.setCursor(30, 30);
+    arduboy.print("Yay!");
+  }
+  
   arduboy.setCursor(0,0);
-  counter = counter + 1;
   arduboy.print(counter);
   arduboy.display();
 
