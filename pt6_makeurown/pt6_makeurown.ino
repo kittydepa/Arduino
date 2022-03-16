725f2f61a51cbc3bdb2f8e6808a7163523bd5877
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2022-03-16 19:55:31 +0100

    initial commit and png files added

---
+++ pt6_makeurown/pt6_makeurown.ino
@@ -0,0 +1,16 @@
+// From community.arduboy user crait
+// Make your own... pt 6 - adding graphics
+
+#include <Arduboy2.h>
+Arduboy2 arduboy;
+
+void setup() {
+  arduboy.begin();
+  arduboy.clear();
+}
+
+void loop() {
+  arduboy.clear();
+
+  arduboy.display();
+}
