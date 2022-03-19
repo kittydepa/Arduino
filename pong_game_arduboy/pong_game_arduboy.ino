83f05db9511b744e0d73eace4803e28da45d4271
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2022-03-19 21:35:37 +0100

    Initial commit for pt 7 - making pong

---
+++ pong_game_arduboy/pong_game_arduboy.ino
@@ -0,0 +1,30 @@
+// Part 7 from user crait, Arduboy Community - Make your own Arduboy game tutorial
+
+
+/*-------------------------------------------------------------------------------------*/
+
+
+// Libraries to include
+#include <Arduboy2.h>
+
+
+// Setting up variables
+Arduboy2 arduboy;
+
+
+/*-------------------------------------------------------------------------------------*/
+
+
+void setup() {
+  
+
+}
+
+
+/*-------------------------------------------------------------------------------------*/
+
+
+void loop() {
+  /
+
+}
