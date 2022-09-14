c0b81d1b49e92f1f3e7672875ddb6f85c00427be
kittydepa <katherine.depa@gmail.com>
2022-09-14 21:13:02 +0200

    TODO list

--- pong_game_arduboy/pong_game_arduboy.ino
+++ pong_game_arduboy/pong_game_arduboy.ino
@@ -1,7 +1,23 @@
 // Part 7 from user crait, Arduboy Community - Make your own Arduboy game tutorial
 
 
-/*-------------------------------------------------------------------------------------*/
+
+/*------------------------------------------
+Break down for creating Pong:
+
+1. Set up a new sketch 
+2. Create a title screen, game screen, win screen, and lose screen
+3. Create a ball that bounces around the screen
+4. Create and control a paddle
+5. Create the computer's paddle
+6. Programming collision
+7. Adjusting the AI
+8. Scoring
+9. Creating a new ball and reseting the games
+
+
+
+-------------------------------------------*/
 
 
 // Libraries to include
